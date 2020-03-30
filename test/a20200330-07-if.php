78dbd17444885cac821a1c7b4bcfa578461d88b5
kobayashi-mother <she426lly@gmail.com>
2020-03-31 00:14:52 +0800

    if

--- test/a20200330-07-if.php
+++ test/a20200330-07-if.php
@@ -1,3 +1,10 @@
+
+
+<?php
+$age=isset($_GET['age'])? intval($_GET['age']):0;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +14,26 @@
 </head>
 <body>
 <?php
-
+if ($age>=18){
+    echo 'welcom!!<br>';
+} else{
+     echo 'none<br>';
+}
 ?>
+
+<?php if ($age>=18){ ?>
+    澳門首家線上賭場上線啦!!
+<h2>恭喜已滿18歲</h2>
+
+<?php } else{ ?>
+請關閉視窗
+<h2>很遺憾閣下年紀未到</h2>
+<?php } ?>
+
+
+
+
+
+
 </body>
 </html>
