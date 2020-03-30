e2ac1a3418574831341d2c11a487a15e26eddd01
kobayashi-mother <she426lly@gmail.com>
2020-03-30 21:41:29 +0800

    + .

--- test/a20200330-02-basic.php
+++ test/a20200330-02-basic.php
@@ -1,4 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 
+$my = 123;
+$a="10";
+$b="abc";
 
+echo $my;
+echo '<br>';
 
+//+只做數值的相加
+echo $my + $a;
+echo '<br>';
+echo $my + $b;
+echo '<br>';
+
+//.做字串的串連
+echo $my. $a;
+echo '<br>';
+echo $my. $b;
+echo '<br>';
+
+?>
+</body>
+</html>
