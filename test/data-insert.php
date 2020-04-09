d830547059ef8ef38e21e1deb445a5cab9fec2c5
帳號名字 <ABC@mail.com>
2020-04-09 17:35:44 +0800

    ALL

--- test/data-insert.php
+++ test/data-insert.php
@@ -1,14 +1,55 @@
+<?php
+require __DIR__. '/__connect_dp.php';
+$page_name = 'data-insert';
+
+if(isset($_POST['name']) and isset($_POST['number'])) {
+
+    $sql = "INSERT INTO `adress_book`(
+`name`,`address`, `mail`, `birthday`, `number`,  `created_at`
+) VALUES (?, ?, ?, ?, ?, NOW())";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        $_POST['name'],
+        $_POST['address'],
+        $_POST['mail'],
+        $_POST['birthday'],
+        $_POST['number'],
+    ]);
+
+    if($stmt->rowCount()==1){
+        $success = true;
+    } else{
+        $success = false;
+    }
+
+
+}
+?>
 
 <?php include __DIR__. '/part-to-php/head.php'; ?>
 <?php include __DIR__. '/part-to-php/navbar.php'; ?>
 
 <div class="container">
+    <?php if(isset($success)): ?>
+    <?php if($success): ?>
+        <div class="alert alert-success" role="alert">
+            資料新增成功
+        </div>
+    <?php else: ?>
+        <div class="alert alert-success" role="alert">
+            資料新增失敗
+        </div>
+        <?php endif; ?>
+    <?php endif; ?>
     <div class="row">
         <div class="col-lg-6">
-            <form>
+            <form method="post">
                 <div class="form-group">
                     <label for="name">name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
+                    <input type="text" class="form-control" id="name" name="name"
+                           value="<?= isset($_POST['name']) ? htmlentities($_POST['name']) : '' ?>">
                     <small id="nameHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
@@ -17,9 +58,9 @@
                     <small id="addressHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
-                    <label for="email">email</label>
-                    <input type="email" class="form-control" id="email" name="email">
-                    <small id="emailHelp" class="form-text text-muted"></small>
+                    <label for="mail">mail</label>
+                    <input type="mail" class="form-control" id="mail" name="mail">
+                    <small id="mailHelp" class="form-text text-muted"></small>
                 </div>
                 <div class="form-group">
                     <label for="birthday">birthday</label>
@@ -28,7 +69,7 @@
                 </div>
                 <div class="form-group">
                     <label for="number">number</label>
-                    <input type="text" class="form-control" id="number" name="number">
+                    <input type="text" class="form-control" id="number" name="number" pattern="09\d{2}-?\d{3}-?\d{3}" required>
                     <small id="numberHelp" class="form-text text-muted"></small>
                 </div>
 
@@ -38,7 +79,5 @@
     </div>
 
 </div>
-
-<?php include __DIR__ . '/parts/html-foot.php'; ?>
-
+<?php include __DIR__. '/part-to-php/script.php'; ?>
 <?php include __DIR__. '/part-to-php/footer.php'; ?>
