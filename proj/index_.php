71f2fd619fbb3ac2d210a946fc065440752477c5
帳號名字 <ABC@mail.com>
2020-04-13 17:51:30 +0800

    ALL

---
+++ proj/index_.php
@@ -0,0 +1,10 @@
+<?php session_start(); ?>
+<?php include __DIR__. '/part-to-php/head.php'; ?>
+<?php include __DIR__. '/part-to-php/navbar.php'; ?>
+<div class="container">
+    <h2>Welcome</h2>
+</div>
+<?php include __DIR__. '/part-to-php/script.php'; ?>
+
+
+<?php include __DIR__. '/part-to-php/footer.php'; ?>
---
+++ proj/part-to-php/script.php
@@ -0,0 +1,4 @@
+
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../bootstrap/js/bootstrap.bundle.js"></script>
+<script src="../fontawesome/js/all.js"></script>
